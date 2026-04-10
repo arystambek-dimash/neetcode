d4a650f2be07ab89ec2e6a7ffb5465a93efbfdde
Dimash <120331171+arystambek-dimash@users.noreply.github.com>
2026-04-10 12:38:30 +0500

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        boxes = [[[] for _ in range(3)] for _ in range(9)]
+        for i in range(0,9):
+            box_row = 0
+            for j in range(0, 9):
+                if j != 0 and j % 3 == 0:
+                    box_row+=1
+                box_col= i // 3
+                if board[i][j] in boxes[box_col][box_row]:
+                    return False
+                if board[i][j].isdigit():
+                    boxes[box_col][box_row].append(board[i][j])
+        formatted = [[] for _ in range(9)]
+        for col in range(0,9):
+            for row in range(0,9):
+                if board[col][row] in formatted[col]:
+                    return False
+                if board[col][row].isdigit():
+                    formatted[col].append(board[col][row])
+        
+        formatted = [[] for _ in range(9)]
+        for col in range(0,9):
+            for row in range(0,9):
+                if board[row][col] in formatted[col]:
+                    return False
+                if board[row][col].isdigit():
+                    formatted[col].append(board[row][col])
+        return True
+        
